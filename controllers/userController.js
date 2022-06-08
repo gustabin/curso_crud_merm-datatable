fcc1894b58e8586418db00665db651998e64d55e
Gustavo Arias <gustavoarias@outlook.com>
2022-06-08 15:15:39 -0300

    edit and delete views

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,3 +16,19 @@ exports.save = (req, res) => {
         }
     });
 };
+
+//procedure to update
+exports.update = (req, res) => {
+    const id = req.body.id
+    const name = req.body.name
+    const email = req.body.email
+    const rol = req.body.rol
+
+    conexion.query('UPDATE users SET ? WHERE id = ?', [{ name:name, email:email, rol:rol}, id ], (error, results) => {
+        if(error) {
+            console.error(error)
+        } else {
+            res.redirect('/');
+        }
+    })
+}
--- routes/router.js
+++ routes/router.js
@@ -20,10 +20,33 @@ router.get('/create', (req, res) => {
     res.render('create')
 })
 
+router.get('/edit/:id', (req, res) => {
+    const id = req.params.id;
+    conexion.query('SELECT * FROM users WHERE id= ?', [id], (error, results) => {
+        if(error){
+            throw error;
+        } else {
+            res.render('edit', { user: results[0] })
+        }
+    })
+})
+
 //to invoke the methods for the CRUD of users
 const userController = require('../controllers/userController')
 
 router.post('/save', userController.save)
+router.post('/update', userController.update)
+
+router.get('/delete/:id', (req, res) => {
+    const id = req.params.id
+    conexion.query('DELETE FROM users WHERE id= ?', [id], (error, results) => {
+        if(error){
+            throw error;
+        } else {
+            res.redirect('/')
+        }
+    })
+});
 
 
 module.exports = router;
