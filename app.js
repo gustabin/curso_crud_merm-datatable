bb55817ad849d4a1b766f8833fc42b08bd06c909
Gustavo Arias <gustavoarias@outlook.com>
2022-06-10 08:54:03 -0300

    custom_template

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.set('view engine', 'ejs')
 
@@ -7,6 +8,8 @@ app.use(express.urlencoded({extended:false}))
 
 // import the router
 app.use('/', require('./routes/router'))
+
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.listen(5000, ()=>{
     console.log('Server running in port: 5000')
