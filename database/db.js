9fa3316e085563c32893ff470cd5273cfdb5c3ba
Gustavo Arias <gustavoarias@outlook.com>
2022-06-17 18:43:42 -0300

    favicon

--- database/db.js
+++ database/db.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql')
 
 const conexion = mysql.createConnection({
-    // host : process.env.DB_HOST,
-    // user : process.env.DB_USER,
-    // password : process.env.DB_PASS,
-    // database : process.env.DB_DATABASE,
-    host : 'www.gustabin.com',
-    user : 'tutornodejs',
-    password : '1234567890',
-    database : 'crud_nodejs',
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_DATABASE,
+    // host : 'www.gustabin.com',
+    // user : 'tutornodejs',
+    // password : '1234567890',
+    // database : 'crud_nodejs',
 })
 
 conexion.connect ((error) => {
